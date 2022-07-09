68b6d7170bfad5d42fbdf5cbcad11c7b0083c9cc
poulomeesinha <poulomee@cennest.com>
2022-07-09 22:01:21 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT|| 5000
+app.use(cors())
+
+app.use(express.json());
+
+const uri = process.env.DB_URI;
+console.log(uri)
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+console.log("MongoDB database connection established successfully");
+})
+app.listen(port,()=>{'server is listening on port',port});
